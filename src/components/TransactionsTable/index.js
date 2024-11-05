44732b6ee59c817da263b07340eb7dc628fe6e79
Hatice AKGÜL <katrancihatice@icloud.com>
2024-11-05 22:32:33 +0300

    select

--- src/components/TransactionsTable/index.js
+++ src/components/TransactionsTable/index.js
@@ -9,7 +9,11 @@ import Paper from '@mui/material/Paper';
 import SearchIcon from '@mui/icons-material/Search';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
-
+import Box from '@mui/material/Box';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Grid from '@mui/material/Grid';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -19,13 +23,10 @@ const Search = styled('div')(({ theme }) => ({
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginBottom: 20,
-  marginLeft: 0,
   width: '100%',
-  [theme.breakpoints.up('sm')]: {
-   
-    width: '50%',
-  },
+  height: '40px',  // Sabit yükseklik ekliyoruz
 }));
+
 const SearchIconWrapper = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 2),
   height: '100%',
@@ -40,19 +41,19 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
+    height: '100%', // Input alanının tüm yüksekliği kaplamasını sağlıyoruz
   },
 }));
 
 function TransactionsTable({ transactions }) {
   const [search, setSearch] = useState('');
+  const [age, setAge] = React.useState('');
 
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
   const columns = [
     { title: "Name", key: "name" },
@@ -62,48 +63,71 @@ function TransactionsTable({ transactions }) {
     { title: "Tag", key: "tag" },
   ];
 
-  let filteredTransactions = transactions.filter((item)=> 
-    item.name.toLowerCase().includes(search.toLocaleLowerCase()))
+  let filteredTransactions = transactions.filter((item) =>
+    item.name.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+
   return (
     <>
-      <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search by name"
-              inputProps={{ 'aria-label': 'search' }}
-              value={search} onChange={(e) => setSearch(e.target.value)} 
-            />
-          </Search>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search by name"
+                inputProps={{ 'aria-label': 'search' }}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </Search>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <FormControl variant="filled" fullWidth size="small" sx={{ height: '40px' }}>
+              <Select
+                labelId="demo-simple-select-filled-label"
+                id="demo-simple-select-filled"
+                value={age}
+                onChange={handleChange}
+                sx={{ height: '100%' }} // Select alanının tam yüksekliği kaplamasını sağlıyoruz
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+      </Box>
 
-
-          <TableContainer component={Paper}>
-     
-     <Table>
-       <TableHead>
-         <TableRow>
-           {columns.map((column) => (
-             <TableCell key={column.key}>{column.title}</TableCell>
-           ))}
-         </TableRow>
-       </TableHead>
-     
-       <TableBody>
-         {filteredTransactions.map((transaction, index) => (
-           <TableRow key={index}>
-             <TableCell>{transaction.name}</TableCell>
-             <TableCell>{transaction.type}</TableCell>
-             <TableCell>{transaction.date}</TableCell>
-             <TableCell>{transaction.amount}</TableCell>
-             <TableCell>{transaction.tag}</TableCell>
-           </TableRow>
-         ))}
-       </TableBody>
-     </Table>
-   </TableContainer>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell key={column.key}>{column.title}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredTransactions.map((transaction, index) => (
+              <TableRow key={index}>
+                <TableCell>{transaction.name}</TableCell>
+                <TableCell>{transaction.type}</TableCell>
+                <TableCell>{transaction.date}</TableCell>
+                <TableCell>{transaction.amount}</TableCell>
+                <TableCell>{transaction.tag}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
-  
   );
 }
 
